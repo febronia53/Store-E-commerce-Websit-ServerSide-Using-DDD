b8d9efd6c931b17a0d4bdad8da711ec7a0f7c0af
Febronia Sameh <fsameh@ejada.com>
2024-02-24 00:17:31 +0200

    Adding auto mapper and value resolver

---
+++ E-commerce.API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using E_commerce.API.Dtos;
+using E_commerceWebsite.AggregateModels.ProductAggregate;
+
+namespace E_commerce.API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductUrlResolver(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+                return _configuration["APIURL"] + source.PictureUrl;
+
+            return null;
+        }
+    }
+}
